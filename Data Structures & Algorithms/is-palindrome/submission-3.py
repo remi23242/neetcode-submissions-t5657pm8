fe08f8b27f137cd57647e03967ef5ed009ea69c3
remi23242 <shoaibrizwan9@gmail.com>
2026-04-12 05:56:45 +0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            # skip non-alphanumeric from left
+            while left < right and not s[left].isalnum():
+                left += 1
+            
+            # skip non-alphanumeric from right
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            # compare characters (case insensitive)
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
+        
+        
